784f7788796fa9456b46b11a1bce7cad2f3300d9
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-26 20:51:13 +0530

    Added:employee attendance

--- Employeewage.java
+++ Employeewage.java
@@ -3,6 +3,12 @@ package wages;
 public class Employeewage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation program");
+		int fulltime=1;
+		double emp=Math.floor(Math.random()*10)%2;
+		if (emp==fulltime)
+			System.out.println("employee is present");
+		else
+			System.out.println("employee is absent");
 	}
 
 }
