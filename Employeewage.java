30ecdaea6541659a1b160ecd7b678d7e6fe6a131
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-26 20:59:39 +0530

    Added:daily wage of employee is calculated

--- Employeewage.java
+++ Employeewage.java
@@ -3,12 +3,19 @@ package wages;
 public class Employeewage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation program");
-		int fulltime=1;
+		int fulltime=1,wagehr=20,dayhrs=8,dailywage;
 		double emp=Math.floor(Math.random()*10)%2;
 		if (emp==fulltime)
+		{
 			System.out.println("employee is present");
+			dailywage=wagehr*dayhrs;
+			System.out.println("dailywage of employee is" +dailywage);
+		}
 		else
+		{
 			System.out.println("employee is absent");
+			System.out.println("daily wage of emplyee is 0");
+		}
 	}
 
 }
