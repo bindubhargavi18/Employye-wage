8f4b8df3f394108e75d2b71e903c5b241b2bedf8
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-26 21:51:22 +0530

    Added:calculated wages per month

--- Employeewage.java
+++ Employeewage.java
@@ -3,20 +3,24 @@ package wages;
 public class Employeewage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation program");
-		int wagehr=20,dayhrs=8,dailywage,parthr=8;
-		int emp=(int) (Math.floor(Math.random()*10)%3);
-		switch(emp)
+		int parttime=2,fulltime=1,wagehr=20,dayhrs=8,dailywage,parthr=8,workingdays=20,monthwage;
+		double emp=(Math.floor(Math.random()*10)%3);
+		if (emp==fulltime)
 		{
-		case 1:
-			dailywage=wagehr*dayhrs;
-			System.out.println("employee daily wage:" +dailywage);
-			break;
-		case 2:
-			dailywage=wagehr*parthr;
-			System.out.println("part time empoyee wage:" +dailywage);
-			break;
-		default:
-			System.out.println("employee is absent and salary will be 0");
+			dailywage=dayhrs*wagehr;
+			monthwage=workingdays*dailywage;
+			System.out.println("employee wages per month:" +monthwage);
+			
+		}
+		else if(emp==parttime)
+		{
+			dailywage=parthr*wagehr;
+			monthwage=workingdays*dailywage;
+			System.out.println("part time employee wage per month:" +monthwage);
+		}
+		else
+		{
+			System.out.println("employee is absent and salary for will be 0");
 		}
 	}
 
